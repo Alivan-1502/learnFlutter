662437b4712b1bd50d3e08968bd8f82feb830236
Mustafa Ali Van <mustafaalivan1502@gmail.com>
2026-02-17 03:46:13 +0300

    learn: create first custom widget screen with scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const IlkUygulamaWidget());
+}
+
+class IlkUygulamaWidget extends StatelessWidget {
+  const IlkUygulamaWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "İlk proje",
       home: Scaffold(
-        appBar: AppBar(title: Text("İlk uygulamam")),
-        body: Material(
+        appBar: AppBar(title: const Text("İlk uygulamam")),
+        body: const Material(
           color: Colors.cyan,
           child: Center(
             child: Text(
-              "Merhaba Flutter",
+              "Hello Flutter!",
               textDirection: TextDirection.ltr,
               style: TextStyle(color: Colors.black, fontSize: 50.0),
-            ), // Text
-          ), // Center
-        ), // Material,
+            ),
+          ),
+        ),
       ),
-    ),
-  ); // MaterialApp
+    );
+  }
 }
